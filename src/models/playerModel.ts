5718dcf352fe3ca833c78a364f354e6ce9b7b19e
pausm99 <pausm99@gmail.com>
2024-03-16 23:36:00 +0100

    defined initial player model

--- src/models/playerModel.ts
+++ src/models/playerModel.ts
@@ -1,3 +1,44 @@
-class Player {}
+import { Model, DataTypes } from 'sequelize';
+import sequelize from '../config/database';
+
+class Player extends Model {}
+
+Player.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        age: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        position: {
+            type: DataTypes.ENUM({
+                values: ['Goalkeeper', 'Defender', 'Midfielder', 'Forward'],
+            }),
+            allowNull: false,
+        },
+        height: {
+            type: DataTypes.DECIMAL,
+            allowNull: false,
+        },
+        width: {
+            type: DataTypes.DECIMAL,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        modelName: 'Player',
+        tableName: 'players',
+        timestamps: false,
+    },
+);
 
 export default Player;
