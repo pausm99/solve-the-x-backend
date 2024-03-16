13c136aad3b88ad54614304a29e4109366ced321
pausm99 <pausm99@gmail.com>
2024-03-17 00:45:58 +0100

    synchronize models to database

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import express, { Express } from "express";
+import sequelize from "./config/database";
 import routes from "./routes/index";
 
 const port = 3000;
 
 const app: Express = express();
+
+app.use(express.json());
+
+
+sequelize.sync({alter: true}).then(() => {
+    console.log('Models synchronized with the database');
+}).catch((error: Error) => {
+    console.error('Error synchronizing models to the database:', error);
+});
+
 
 app.use('/', routes);
 
