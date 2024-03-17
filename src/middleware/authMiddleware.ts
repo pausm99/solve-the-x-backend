bfcceaa873a29668b9d32570cdbf7c110d51a708
pausm99 <pausm99@gmail.com>
2024-03-17 20:09:14 +0100

    created function to authenticate token used on players route

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,4 +1,5 @@
 import jwt, { Secret } from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
 import User from '../models/userModel';
 
 const secretKey: Secret | undefined = process.env.SECRET_KEY;
@@ -12,4 +13,28 @@ function generateAuthToken(user: User): string {
     return jwt.sign(payload, secretKey, { expiresIn: '2h' });
 }
 
-export { generateAuthToken };
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, consistent-return
+function authenticateToken(req: Request, res: Response, next: NextFunction) {
+    try {
+        const token = req.headers.authorization?.split(' ')[1];
+  
+        if (!token) {
+            return res.status(401).json({ message: 'Unauthorized, token not provided' });
+        }
+
+        if (!secretKey) throw new Error('secretKey is no defined');
+  
+        // eslint-disable-next-line consistent-return
+        jwt.verify(token, secretKey, (err) => {
+            if (err) {
+                return res.status(403).json({ message: 'Invalid token' });
+            }
+            next();
+        });
+    } catch (error) {
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+
+export { generateAuthToken, authenticateToken };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import playerRoutes from './playerRoutes';
 import userRoutes from './userRoutes';
+import { authenticateToken } from '../middleware/authMiddleware';
 
 const router = express.Router();
 
 
-router.use('/players', playerRoutes);
+router.use('/players', authenticateToken, playerRoutes);
 router.use('/users', userRoutes);
 
 export default router;
