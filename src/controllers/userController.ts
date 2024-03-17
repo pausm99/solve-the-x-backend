7f257c6944f3b44290d3f03d1a2357b95389a089
pausm99 <pausm99@gmail.com>
2024-03-17 16:37:28 +0100

    added register endpoint and function (now returning token when logged instead of when registered)

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -18,17 +18,38 @@ export class UserController {
 
             const hashedPassword = await bcrypt.hash(password, 10);
 
-            const newUser = await User.create({
+            await User.create({
                 username,
                 password: hashedPassword,
             });
 
-            const token = generateAuthToken(newUser);
-
-            return res.status(201)
-                .header('Authorization', `Bearer ${token}`)
-                .json({ message: 'User registered successfully' });
+            return res.status(201).json({ message: 'User registered successfully' });
                 
+        } catch (error) {
+            return res.status(500).json({ message: 'Error creating user', error });
+        }
+    }
+
+    async login(req: Request, res: Response): Promise<Response<string>> {
+        try {
+            const { username, password } = req.body;
+
+            const user = await User.findByPk(username);
+
+            if (!user){
+                return res.status(401).json({ message: 'User not found' });
+            }
+
+            const passwordValid = await bcrypt.compare(password, user!.password);
+            if (!passwordValid) {
+                return res.status(401).json({ message: 'Invalid credentials' });
+            }
+            else {
+                const token = generateAuthToken(user);
+                return res.status(200)
+                    .header('Authorization', `Bearer ${token}`)
+                    .json({ message: 'User logged successfully' });
+            }
         } catch (error) {
             return res.status(500).json({ message: 'Error creating user', error });
         }
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -4,6 +4,7 @@ const router = express.Router();
 
 
 router.route('/register').post(authController.register);
+router.route('/login').post(authController.login);
 
 
 export default router;
