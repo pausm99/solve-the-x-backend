eb2b8336fc8cf6c019d6829d7dbab88ddf2b2af1
pausm99 <pausm99@gmail.com>
2024-03-19 17:04:11 +0100

    added statsRoute to routes with autentication

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import playerRoutes from './playerRoutes';
 import userRoutes from './userRoutes';
+import statsRoutes from './statsRoutes';
 import { authenticateToken } from '../middleware/authMiddleware';
 
 const router = express.Router();
@@ -8,5 +9,6 @@ const router = express.Router();
 
 router.use('/players', authenticateToken, playerRoutes);
 router.use('/users', userRoutes);
+router.use('/statistics', authenticateToken, statsRoutes);
 
 export default router;
